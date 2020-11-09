f2374b793c9c5e84c34a81ddee5c454ce81b3373
Carassale <Carassale@users.noreply.github.com>
2020-11-09 09:42:13 +0000

    Deploying to gh-pages from  @ c27156287f204f100e94a6b15a45e55bfbfb3909 🚀

--- api_project.js
+++ api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-09T09:37:48.416Z",
+    "time": "2020-11-09T09:42:11.670Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
