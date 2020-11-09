871008c563d7929a4a52bacabbfd397f92677fbd
Carassale <Carassale@users.noreply.github.com>
2020-11-09 09:16:09 +0000

    Deploying to gh-pages from  @ aff6b5b000c4f373a57b57e571b558546193d0f2 🚀

---
+++ api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "Corona Virus Numbers - API",
+  "version": "1.0.0",
+  "description": "Corona Virus Numbers APIs Documentation",
+  "title": "Corona Virus Numbers",
+  "url": "",
+  "template": {
+    "withCompare": false,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-11-09T09:16:03.595Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
